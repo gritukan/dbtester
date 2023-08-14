0384babd27a3f0f27147b60ffbafab87e6fa2098
Grigory Reznikov <gritukan@ytsaurus.tech>
2023-08-14 13:19:51 +0300

    wip

--- scripts/install-go.sh
+++ scripts/install-go.sh
@@ -8,17 +8,13 @@ sudo rm -f /usr/local/go/bin/go && sudo rm -rf /usr/local/go && sudo rm -f /bin/
 GOOGLE_URL=https://go.dev/dl
 DOWNLOAD_URL=${GOOGLE_URL}
 
-sudo curl -F -s ${DOWNLOAD_URL}/go$GO_VERSION.linux-amd64.tar.gz | sudo tar -v -C /usr/local/ -xz
+sudo curl -L -s ${DOWNLOAD_URL}/go$GO_VERSION.linux-amd64.tar.gz | sudo tar -v -C /usr/local/ -xz
 
-if grep -q GOPATH "$(echo $HOME)/.bashrc"; then
-  echo "bashrc already has GOPATH";
-else
-  echo "adding GOPATH to bashrc";
-  echo "export GOPATH=$(echo $HOME)/go" >> $HOME/.bashrc;
-  PATH_VAR=$PATH":/usr/local/go/bin:$(echo $HOME)/go/bin";
-  echo "export PATH=$(echo $PATH_VAR)" >> $HOME/.bashrc;
-  source $HOME/.bashrc;
-fi
+echo "adding GOPATH to bashrc";
+echo "export GOPATH=$(echo $HOME)/go" >> $HOME/.bashrc;
+PATH_VAR=$PATH":/usr/local/go/bin:$(echo $HOME)/go/bin";
+echo "export PATH=$(echo $PATH_VAR)" >> $HOME/.bashrc;
+source $HOME/.bashrc;
 
 mkdir -p $GOPATH/bin/
 source $HOME/.bashrc
@@ -28,3 +24,4 @@ unset GOROOT
 echo $GOPATH
 export GOROOT=/usr/local/go
 go version
+
